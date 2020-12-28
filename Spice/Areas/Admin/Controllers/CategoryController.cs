d69e21cc4c81cbb52b276ba6d8bb53267de787c8
ebn huz <ebnhgs@gmail.com>
2020-12-28 14:04:18 -0500

    Add GET Details

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -111,6 +111,22 @@ namespace Spice.Areas.Admin.Controllers
         }
 
 
+        //GET - Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+
     }
 
 
