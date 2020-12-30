08019fe2962d32f4cd9f7128794cdea3b7f16baa
ebn huz <ebnhgs@gmail.com>
2020-12-29 21:02:44 -0500

    GET Create And View MenuItem

---
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+
+        public MenuItemController(ApplicationDbContext db, IWebHostEnvironment hostingEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostingEnvironment;
+            MenuItemVM = new MenuItemViewModel
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var MenuItems = await _db.MenuItem.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync();
+            return View(MenuItems);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
+        }
+    }
+}
