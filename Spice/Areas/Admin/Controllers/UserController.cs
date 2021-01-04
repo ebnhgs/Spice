ac9081b15e8ff53da1ed7ddcb5e5a167aa9833d3
ebn huz <ebnhgs@gmail.com>
2021-01-04 15:47:57 -0500

    User Add Lockout Function

--- Spice/Areas/Admin/Controllers/UserController.cs
+++ Spice/Areas/Admin/Controllers/UserController.cs
@@ -26,5 +26,53 @@ namespace Spice.Areas.Admin.Controllers
             
             return View(await _db.ApplicationUser.Where(u=>u.Id != claim.Value).ToListAsync());
         }
+
+
+
+        public async Task<IActionResult> Lock(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _db.ApplicationUser.Where(u => u.Id == id).FirstOrDefaultAsync();
+
+            if (applicationUser==null)
+            {
+                return NotFound();
+            }
+
+            applicationUser.LockoutEnd = DateTime.Now.AddYears(1000);
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public async Task<IActionResult> UnLock(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _db.ApplicationUser.Where(u => u.Id == id).FirstOrDefaultAsync();
+
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            applicationUser.LockoutEnd = DateTime.Now;
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
     }
 }
