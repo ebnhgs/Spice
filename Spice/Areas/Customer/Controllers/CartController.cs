eac75f760c9f16ef088781889a8f4880139f65b2
ebn huz <ebnhgs@gmail.com>
2021-01-05 18:44:04 -0500

    Cart Controller Index_OrderDetailsCartVM

---
+++ Spice/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models.ViewModels;
+using Spice.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Spice.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public OrderDetailsCart detailCart { get; set; }
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+        public async Task<IActionResult> Index()
+        {
+            //added detail cart and zeroed out order total
+            detailCart = new OrderDetailsCart()
+            {
+                OrderHeader = new Models.OrderHeader()
+            };
+
+            detailCart.OrderHeader.OrderTotal = 0;
+
+            //Retried user id of logged in user
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            //pull in users shopping cart
+            var cart = _db.ShoppingCart.Where(c => c.ApplicationUserId == claim.Value);
+        
+            
+            if(cart != null)
+            {
+                //if user's cart has menu items in it, add to the cart those items stored in the Db for this user
+                detailCart.ListCart = cart.ToList();
+            }
+
+            foreach(var list in detailCart.ListCart)
+            {
+                //if user's cart does not have menu items in it, then iterate over menu items user has added to cart
+                list.MenuItem = await _db.MenuItem.FirstOrDefaultAsync(m => m.Id == list.MenuItemId);
+                //update order total each time menu item is iterated over
+                detailCart.OrderHeader.OrderTotal = detailCart.OrderHeader.OrderTotal + (list.MenuItem.Price * list.Count);
+                //convert description to raw html   
+                list.MenuItem.Description = SD.ConvertToRawHtml(list.MenuItem.Description);
+                if (list.MenuItem.Description.Length > 100)
+                {
+                    list.MenuItem.Description = list.MenuItem.Description.Substring(0, 99) + "...";
+                }   
+            }
+
+            //set order total original to order total prior to user adding any coupons
+            detailCart.OrderHeader.OrderTotalOriginal = detailCart.OrderHeader.OrderTotal;
+
+            return View(detailCart);
+
+        
+        }
+    }
+}
