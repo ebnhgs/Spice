2a73c442e22d3b9cf983ac2023aad69b92c4484c
ebn huz <ebnhgs@gmail.com>
2021-01-05 10:06:17 -0500

    Customer Details View

--- Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Spice.Data;
@@ -40,6 +41,21 @@ namespace Spice.Controllers
             }; 
 
             return View(IndexVM);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Details (int id)
+        {
+            var menuItemFromDb = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).Where(m => m.Id == id).FirstOrDefaultAsync();
+
+            ShoppingCart cartObj = new ShoppingCart()
+            {
+                MenuItem = menuItemFromDb,
+                MenuItemId = menuItemFromDb.Id
+            };
+
+            return View(cartObj);
+
         }
 
         public IActionResult Privacy()
